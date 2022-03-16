644cb8822703caeda944743962d54901492885e2
jacob ramirez <jacobnramirez@gmail.com>
2022-03-15 19:03:08 -0700

    added object and its properties

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// let generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   let password = generatePassword();
+//   let passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
+// }
+
+// // Add event listener to generate button
+// // generateBtn.addEventListener("clik writePassword);
+
+
+let password = {
+  length: (8-128),
+  letters: ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+  number: ("0123456789"),
+  special: ("!#$%&'()*-./;:<=>?@[]^_`{|}~"),
+  // unable to place \ as well as " (in sepcialChar)
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
