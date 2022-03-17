b725b921d2f471dddea235dbedcb08748dda44db
jacob ramirez <jacobnramirez@gmail.com>
2022-03-17 14:58:48 -0700

    reset arrays removed object format

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 // // Assignment Code
-// let generateBtn = document.querySelector("#generate");
+ let generateBtn = document.querySelector("#generate");
+ let lengthOfPassword = 8;
+ let lettersLower = ['abcdefghijklmnopqrstuvwxyz'];
+ let lettersUpper = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+ let theNumbers = ['0123456789']; 
+ let specialChar =['!#$%&()*-./;:<=>?@][^_`{|}~"'];
+ let userChoice = [];
+
+
+ function generatePassword() {
+   console.log("button works");
+
+   
+   return "generated passwork will go here";
+ }
 
 // // Write password to the #password input
-// function writePassword() {
-//   let password = generatePassword();
-//   let passwordText = document.querySelector("#password");
+ function writePassword() {
+   let password = generatePassword();
+   let passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+   passwordText.value = password;
+
+ }
+
+ //Add event listener to generate button 
+ generateBtn.addEventListener("click", writePassword);
+
+
+
+
+//   // unable to place \ as well as " (in sepcialChar)
+
+
+// let prompts = {
+//   promptLength: ("How long would you like your password to be?"),
+//   promptLetter: ("Would you like to include + //letters, numbers, special)")
 
 // }
 
-// // Add event listener to generate button
-// // generateBtn.addEventListener("clik writePassword);
+// function writePassword() {
+//     let password = generatePassword();
+//     let passwordText = document.querySelector("#password");
+//     prompts.promptLength; 
+//     return; 
+  
+//     passwordText.value = password;
 
-
-let password = {
-  length: (8-128),
-  letters: ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
-  number: ("0123456789"),
-  special: ("!#$%&'()*-./;:<=>?@[]^_`{|}~"),
-  // unable to place \ as well as " (in sepcialChar)
-}
+//   
